d06db2d95cc8411667a61b92d7605c672d5b8761
cdfhalle <conradhalle@gmail.com>
2021-11-21 14:53:59 +0100

    closes #5

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,15 +1,23 @@
 class AuthorsController < ApplicationController
+    def index
+        @authors = Author.all
+    end
+
     def show
         @author = Author.find(params[:id])
     end
 
     def new
+        @author = Author.new
     end
 
     def create
         @author = Author.new(author_params)
-        @author.save
-        redirect_to @author
+        if @author.save
+            redirect_to @author
+        else
+            render 'new'
+        end
     end
     private
         def author_params
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+    validates :first_name, presence: true, length: {maximum: 50, minimum: 2}
+    validates :last_name, presence: true, length: {maximum: 50, minimum: 2}
+    validates :homepage, presence: true, length: {maximum: 50}
+
+    def name
+        first_name + ' ' + last_name      
+    end
 end

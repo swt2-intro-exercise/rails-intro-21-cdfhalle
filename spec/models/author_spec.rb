45e6a3412bc52e1e7ce4fbd933ff014fb2dc1faf
cdfhalle <conradhalle@gmail.com>
2021-11-22 15:28:12 +0100

    test for #6

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,14 +1,14 @@
 describe "New author instance", type: :model do
-
-    author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
-
+    before do
+        @author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+    end
     it "schould exist after creation and contain first_name, last_name and homepage" do
-        expect(author.first_name).to eq('Alan')
-        expect(author.last_name).to eq('Turing')
-        expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+        expect(@author.first_name).to eq('Alan')
+        expect(@author.last_name).to eq('Turing')
+        expect(@author.homepage).to eq('http://wikipedia.org/Alan_Turing')
     end
     
     it "should have a method called name that returns the full name of the author" do
-        expect(author.name).to eq('Alan Turing')
+        expect(@author.name).to eq('Alan Turing')
     end
 end

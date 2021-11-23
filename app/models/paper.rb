88d812721a1bc1eae0295359564586f224af12f6
cdfhalle <conradhalle@gmail.com>
2021-11-23 19:53:45 +0100

    next try

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,5 @@
 class Paper < ApplicationRecord
-    has_and_belongs_to_many :author, join_table: "authors_papers", foreign_key: "author_id"
+    has_and_belongs_to_many :authors, join_table: "authors_papers", foreign_key: "author_id"
     validates :title, presence: true
     validates :venue, presence: true
     validates :year, presence: true, numericality: true     

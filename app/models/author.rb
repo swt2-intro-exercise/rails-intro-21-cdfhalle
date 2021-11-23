082f99135c5d25fe2f980287c485b1bc76c4f1fe
cdfhalle <conradhalle@gmail.com>
2021-11-23 19:50:43 +0100

    ...

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ApplicationRecord
+    has_and_belongs_to_many :paper, join_table: "authors_papers", foreign_key: "paper_id"
     validates :first_name, presence: true, length: {maximum: 50, minimum: 2}
     validates :last_name, presence: true, length: {maximum: 50, minimum: 2}
     validates :homepage, presence: true, length: {maximum: 50}
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,5 @@
 class Paper < ApplicationRecord
+    has_and_belongs_to_many :author, join_table: "authors_papers", foreign_key: "author_id"
     validates :title, presence: true
     validates :venue, presence: true
     validates :year, presence: true, numericality: true     
